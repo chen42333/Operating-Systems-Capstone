cae29e1f4d155d0fb0e1b82e01ce958ff58d6c94
chen42333 <cch92423@gmail.com>
2025-03-12 23:34:48 +0800

    read kernel to _skernel directly in the bootloader

--- src/bootloader/main.c
+++ src/bootloader/main.c
@@ -1,5 +1,4 @@
 #include "uart.h"
-#include "utils.h"
 
 #define MAGIC 0x544f4f42
 
@@ -22,11 +21,10 @@ int main(void *_dtb_addr)
         uart_read(buf, 4, RAW_MODE);
         checksum = *(uint32_t*)buf;
 
-        char data[data_len];
-        uart_read(data, data_len, RAW_MODE);
+        uart_read(_skernel, data_len, RAW_MODE);
         
         for (int i = 0; i < data_len; i++)
-            sum += (uint32_t)(unsigned char)data[i];
+            sum += (uint32_t)(unsigned char)_skernel[i];
 
         if (magic != MAGIC)
             uart_write_string("Invalid file\r\n");
@@ -35,7 +33,6 @@ int main(void *_dtb_addr)
         else
         {
             uart_write_string("File received\r\n");
-            memcpy((void*)_skernel, (void*)data, data_len);
             asm volatile ("mov x0, %0" :: "r"(_dtb_addr));
             asm volatile ("b _skernel");
             break;
